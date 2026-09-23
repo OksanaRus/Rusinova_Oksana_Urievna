91a096022bcf6a1174fb1adc6ce8b64681a50eba
Oksana Rusinova <56554357+OksanaRus@users.noreply.github.com>
2026-09-23 14:58:48 +0500

    Add Framer export files

---
+++ utils-MaUy.DSbAkNwf.mjs
@@ -0,0 +1,2 @@
+import{t as e}from"./rolldown-runtime.Dh6celcD.mjs";import{O as t}from"./shared-lib.3nvi2RU3.mjs";var n=e((()=>{t()}));export{n as t};
+//# sourceMappingURL=utils-MaUy.DSbAkNwf.mjs.map
